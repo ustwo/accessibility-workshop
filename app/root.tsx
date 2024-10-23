ea7dd701c1ffd0d7c2f003ec576f8db5fe26219e
romansharapov19 <roman.s.19@hotmail.com>
2024-10-23 11:13:37 +0100

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,21 @@
+import appStylesHref from './app.css?url';
+
+import { ScrollRestoration, Links, Meta, Scripts } from '@remix-run/react';
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <p>Hello world!</p>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { vitePlugin as remix } from '@remix-run/dev';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    remix({
+      ignoredRouteFiles: ['**/*.css'],
+    }),
+    tsconfigPaths(),
+  ],
+});
