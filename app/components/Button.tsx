a698bb8a167e70f04a65e0a9dda65f71e28b562b
romansharapov19 <roman.s.19@hotmail.com>
2024-11-04 18:09:39 +0000

    feat: create authenticated navigation header

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -13,6 +13,10 @@ const buttonVariants = {
     fontSize: '17px',
     lineHeight: '22.95px',
   },
+  clean: {
+    border: 'none',
+    backgroundColor: 'transparent'
+  }
 };
 export const Button = ({ children, variant, ...props }: ButtonProps) => {
   return (
